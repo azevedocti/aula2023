80909de8bc8e781d5b768e59a13c8459973cc3ba
azevedocti <136394814+azevedocti@users.noreply.github.com>
2023-09-13 21:26:15 -0300

    2 files modified

--- src/components/filme/filme.tsx
+++ src/components/filme/filme.tsx
@@ -15,6 +15,7 @@ export function Filme({filme} : FilmeProps){
   <>
   <div className="filme">
   <img src={"https://image.tmdb.org/t/p/w154" + filme.poster_path} />
+  <small>{filme.vote_average}</small>
   <div> {filme.original_title}</div>
   </div>
   </>
